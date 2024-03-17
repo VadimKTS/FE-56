53d04cd937b000676cf4708dda6a41f247de6d1c
VadimKTS <123896357+VadimKTS@users.noreply.github.com>
2024-03-18 00:22:26 +0300

    feat: add cardRender

--- app-course-work/js/index.js
+++ app-course-work/js/index.js
@@ -4,6 +4,33 @@ import {getUserList, getUserById} from './user.js';
 // Запускаем clock
 currentTime();
 
+// Отображаем карточки
+const allCards = JSON.parse(localStorage.getItem('cards'));
+if (!allCards) {
+  LScardList = [];
+}
+//----------------------
+function renderAllCards(cards) {
+  const cardList = document.querySelector('.cards-list');
+  cards.forEach(card => {
+    cardList.innerHTML += `<li class="card">
+    <div class="cadr-body">
+      <h3>${card.title}</h3>
+      <p>${card.description}</p>
+      <span class="user">${card.user}</span>
+      <span class="time">${card.time}</span>
+    </div>
+    <div class="card-buttons">
+      <button class="edit" id="openModal">EDIT</button>
+      <button class="delete">DELETE</button>
+    </div>
+  </li>`;
+  });
+}
+//---
+renderAllCards(allCards);
+
+//==========================
 class ItcModal {
   #template = `
   <div class="modal" id="myModal">
@@ -60,17 +87,9 @@ users.then(users => {
         description: card.description,
         options: users.map(user => `<option id="${user.id}">${user.name}</option>`).join(''),
       });
-      //modal.render();
     }
   });
 
-  // const modal = new ItcModal({
-  //   title: 'Заголовок',
-  //   description: 'Описание модального окна...',
-  //   options: users.map(user => `<option id="${user.id}">${user.name}</option>`).join(''),
-  //   footerButtons: [{class: 'btn btn-close', text: 'Закрыть', action: 'close'}],
-  // });
-  // console.log(modal);
   const open = document.getElementById('openModal');
   open.addEventListener('click', () => {
     modal.render();
@@ -88,13 +107,14 @@ users.then(users => {
         title: title,
         description: description,
         user: user,
+        time: new Date().toLocaleTimeString(),
       };
 
       let cardList = JSON.parse(localStorage.getItem('cards'));
       if (!cardList) {
         cardList = [];
       }
-      cardList[indexCard] = card; // fix logic
+      cardList[indexCard] = card; // Обновляем данные
       localStorage.setItem('cards', JSON.stringify(cardList)); // Сохраняем данные
       modal.remove();
     });
@@ -105,81 +125,82 @@ users.then(users => {
 });
 
 //=====================
-// class AddCardModal {
-//   #template = `
-//   <div class="modal" id="addCardModal">
-//   <div class="modal-content">
-//     <input class="modal-title" id="title" value="{{title}}">   Title</input>
-//     <input class="modal-description" id="description" value="{{description}}">   Description</input>
-//     <select class="modal-select" id="select">
-//     {{options}}
-//     </select>
-//     <div class="modal-buttons">
-//       <button class="modal-button add-card-modal-button-cancel" >Отмена</button>
-//       <button class="modal-button add-card-modal-button-confirm" >Подтвердить</button>
-//     </div>
-//   </div>
-// </div>
-// `;
+class AddCardModal {
+  #template = `
+  <div class="modal" id="addCardModal">
+  <div class="modal-content">
+    <input class="modal-title" id="title" value="{{title}}">   Title</input>
+    <input class="modal-description" id="description" value="{{description}}">   Description</input>
+    <select class="modal-select" id="select">
+    {{options}}
+    </select>
+    <div class="modal-buttons">
+      <button class="modal-button add-card-modal-button-cancel" >Отмена</button>
+      <button class="modal-button add-card-modal-button-confirm" >Подтвердить</button>
+    </div>
+  </div>
+</div>
+`;
 
-//   constructor({title, description, options} = {}) {
-//     this.title = title;
-//     this.description = description;
-//     this.options = options;
-//   }
-//   render() {
-//     const modal = document.createElement('div');
+  constructor({title, description, options} = {}) {
+    this.title = title;
+    this.description = description;
+    this.options = options;
+  }
+  render() {
+    const modal = document.createElement('div');
 
-//     modal.innerHTML = this.#template
-//       .replace('{{title}}', this.title)
-//       .replace('{{description}}', this.description)
-//       .replace('{{options}}', this.options);
-//     document.body.append(modal);
-//   }
+    modal.innerHTML = this.#template
+      .replace('{{title}}', this.title)
+      .replace('{{description}}', this.description)
+      .replace('{{options}}', this.options);
+    document.body.append(modal);
+  }
 
-//   remove() {
-//     const modal = document.getElementById('addCardModal');
-//     modal.remove();
-//   }
-// }
+  remove() {
+    const modal = document.getElementById('addCardModal');
+    modal.remove();
+  }
+}
 
-// //const users = getUserList();
-// users.then(users => {
-//   const modal = new ItcModal({
-//     title: 'Заголовок',
-//     description: 'Описание модального окна...',
-//     options: users.map(user => `<option id="${user.id}">${user.name}</option>`).join(''),
-//     footerButtons: [{class: 'btn btn-close', text: 'Закрыть', action: 'close'}],
-//   });
-//   console.log(modal);
-//   const open = document.getElementById('openModal');
-//   open.addEventListener('click', () => {
-//     modal.render();
+//const users = getUserList();
+users.then(users => {
+  const modal = new AddCardModal({
+    title: 'Заголовок',
+    description: 'Описание модального окна...',
+    options: users.map(user => `<option id="${user.id}">${user.name}</option>`).join(''),
+    footerButtons: [{class: 'btn btn-close', text: 'Закрыть', action: 'close'}],
+  });
+  console.log(modal);
+  const open = document.getElementById('addTodoModal');
+  open.addEventListener('click', () => {
+    modal.render();
 
-//     const confirm = document.querySelector('.modal-button-confirm');
-//     confirm.addEventListener('click', () => {
-//       const title = document.getElementById('title').value;
-//       const description = document.getElementById('description').value;
-//       const select = document.getElementById('select');
-//       const option = select.options[select.selectedIndex];
-//       const user = option.id;
+    const confirm = document.querySelector('.add-card-modal-button-confirm');
+    confirm.addEventListener('click', () => {
+      const title = document.getElementById('title').value;
+      const description = document.getElementById('description').value;
+      const select = document.getElementById('select');
+      const option = select.options[select.selectedIndex];
+      const user = option.id;
 
-//       const card = {
-//         id: Date.now(),
-//         title: title,
-//         description: description,
-//         user: user,
-//       };
+      const card = {
+        id: Date.now(),
+        title: title,
+        description: description,
+        user: user,
+      };
 
-//       let cardList = JSON.parse(localStorage.getItem('cards'));
-//       if (!cardList) {
-//         cardList = [];
-//       }
-//       cardList.push(card);
-//       localStorage.setItem('cards', JSON.stringify(cardList)); // Сохраняем данные
-//     });
+      let cardList = JSON.parse(localStorage.getItem('cards'));
+      if (!cardList) {
+        cardList = [];
+      }
+      cardList.push(card);
+      localStorage.setItem('cards', JSON.stringify(cardList)); // Сохраняем данные
+      modal.remove();
+    });
 
-//     const close = document.querySelector('.modal-button-cancel');
-//     close.addEventListener('click', () => modal.remove());
-//   });
-// });
+    const close = document.querySelector('.add-card-modal-button-cancel');
+    close.addEventListener('click', () => modal.remove());
+  });
+});
